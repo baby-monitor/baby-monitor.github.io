5728927af9ef100145e8c8ad7ec969c775cd18a2
Andrew Lee <andrew.lee.cal@gmail.com>
2022-09-26 20:27:04 -0700

    Set up webpack for multipage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-type State = [string];
+import {Peer} from 'peerjs';
 
-let state: [""]
+type State = {
+    type: "home";
+} | {
+    type: "camera";
+    id: string;
+} | {
+    type: "viewer"
+    id: string;
+};
 
-console.log("hi there");
+const salty = "32d52c1c-5ea3-4ec2-8681-9f4808375501"; // TODO: Remove hardcoded ID and use generated QR code
 
+let state: State;
+const hash = window.location.hash;
+
+function getStateFromHash(hash: string): State | undefined {
+    //if 
+    return undefined;
+}
+
+if (!hash) {
+    window.location.hash = "#home";
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  "entry": "./src/index.ts",
+  "entry": {
+    "index": "./src/index.ts",
+    "camera": "./src/scripts/camera.ts",
+  },
   "module": {
     "rules": [
       {
@@ -18,10 +21,19 @@ module.exports = {
   "plugins": [
     new HtmlWebpackPlugin({
       title: "Baby Monitor",
+      template: "./src/index.html",
+      chunks: ["index"],
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      title: "Baby Monitor",
+      template: "./src/camera.html",
+      chunks: ["camera"],
+      filename: "camera.html",
     }),
   ],
   "output": {
-    "filename": "main.js",
+    //"filename": "main.js",
     "path": path.resolve(__dirname, "dist"),
   }
 }
